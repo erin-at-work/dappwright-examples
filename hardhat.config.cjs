5d03ed8d761ec697f06e4c23626801b375fcea66
Erin Lee <erin.lee@coinbase.com>
2023-04-05 13:44:46 -0400

    Changes to port & option to use forked network

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,4 +1,11 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  // networks: {
+  //   hardhat: {
+  //     forking: {
+  //       url: "https://mainnet.infura.io/v3/<INFURA_API_KEY>",
+  //     },
+  //   },
+  // },
 };
